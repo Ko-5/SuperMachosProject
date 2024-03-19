ea8d15c9567a2677996039f1695faf8bb1512739
KO-5 <guilhermea.silva.55@gmail.com>
2024-03-19 15:44:32 -0400

    a

--- SuperMachos.py
+++ SuperMachos.py
@@ -1,1 +1,14 @@
-print('hello world')
+from random import shuffle, choice
+
+class trunfo:
+    def __init__(self,id,name,life,money,reign) -> None:
+        self.id = id
+        self.name = name
+        self.life = life
+        self.money = money
+        self.reign = reign
+
+    def setPack(cartas):
+        shuffle(cartas)
+        return cartas
+    
