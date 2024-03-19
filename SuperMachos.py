663168da1a3d05f2a30d07c5cee832c0c36ba655
KO-5 <guilhermea.silva.55@gmail.com>
2024-03-19 16:33:59 -0400

    a

--- SuperMachos.py
+++ SuperMachos.py
@@ -1,8 +1,7 @@
 from random import shuffle, choice
 
 class trunfo:
-    def __init__(self,id,name,life,money,reign) -> None:
-        self.id = id
+    def __init__(self,name,life,money,reign) -> None:
         self.name = name
         self.life = life
         self.money = money
@@ -12,3 +11,5 @@ class trunfo:
         shuffle(cartas)
         return cartas
     
+A2 = trunfo('Napoleão Bonaparte', 52, 160000000000, 10)
+A4 = trunfo('Cleópata', 39, 9800000000000,21)
